64f4a05467d270938543e003ec0127f2d39e5eef
Anu-01-rb <anu.01rob@outlook.com>
2024-08-29 22:35:59 +0700

    Complete find odd number

--- odd_number.go
+++ odd_number.go
@@ -2,4 +2,17 @@ package main
 
 func FindOddNumber(text []int) int {
 	// TODO : start your code here
+	key := make(map[int]int)
+
+	for _, num := range text {
+		key[num]++
+	}
+
+	for num, count := range key {
+		if count%2 == 1 {
+			return num
+		}
+	}
+
+	return 0
 }
