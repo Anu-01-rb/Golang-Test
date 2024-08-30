b36d5f1dad1a7f5ab965b1dbe78daf1df8f223b5
Anu-01-rb <anu.01rob@outlook.com>
2024-08-30 10:20:12 +0700

    Complete code and test for smiley

--- smily.go
+++ smily.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"strings"
+)
+
+func CheckContain(text string, sign1 string, sign2 string) bool {
+	if strings.Contains(text, sign1) || strings.Contains(text, sign2) {
+		return true
+	}
+	return false
+}
 
 func CountSmilyFace(text []string) int {
-    // TODO : start your code here
+	// TODO : start your code here
+	smiles := 0
+	for _, v := range text {
+		if len(v) == 3 {
+			pos1 := CheckContain(string(v[0]), ":", ";")
+			pos2 := CheckContain(string(v[1]), "-", "~")
+			pos3 := CheckContain(string(v[2]), ")", "D")
+			if pos1 && pos2 && pos3 {
+				smiles++
+			}
+		} else if len(v) == 2 {
+			pos1 := CheckContain(string(v[0]), ":", ";")
+			pos3 := CheckContain(string(v[1]), ")", "D")
+			if pos1 && pos3 {
+				smiles++
+			}
+		}
+
+	}
+	return smiles
 }
--- smily_test.go
+++ smily_test.go
@@ -1,3 +1,38 @@
 package main
 
-// Write your test here
+import "testing"
+
+func TestCountSmilyFace(t *testing.T) {
+	type args struct {
+		text []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "1st Case",
+			args: args{[]string{":)", ";(", ";}", ":-D"}},
+			want: 2,
+		},
+		{
+			name: "2nd Case",
+			args: args{[]string{";D", ":-(", ":-)", ";~)"}},
+			want: 3,
+		},
+		{
+			name: "3rd Case",
+			args: args{[]string{";]", ":[", ";*", ":$", ";-D"}},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CountSmilyFace(tt.args.text); got != tt.want {
+				t.Errorf("CountSmilyFace() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
